5299181c36dd21e79615e411a5d8a64c2fefeba9
Карнаухов Всеволод <112627329+kgmonarqez@users.noreply.github.com>
2025-04-06 18:54:08 +0300

    Admin edit page
    Страница админа "Редактировать"

--- front/fire-station/src/router/index.js
+++ front/fire-station/src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/LoginView.vue'
 import UserProfileView from "../views/UserProfileView.vue"
+import AdminEditView from '@/views/AdminEditView.vue'
 
 
 const routes = [
   { path: '/', name: 'Login', component: HomeView },
   { path: '/userprofile', name: 'UserProfile', component: UserProfileView },
   //{ path: '/reports', name: 'Reports', component: ReportsView },
+  { path: '/edit', name: 'AdminEdit', component: AdminEditView},
 ]
 
 const router = createRouter({
