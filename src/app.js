ae7b0f5e7a378123cbaa9c2ae00ebcc3e0cd9109
EfremovaAnna <efremova.anna.vyacheslavovna@gmail.com>
2025-04-11 16:09:51 +0300

    fix generate

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import path from "path";
 import dotenv from "dotenv";
 import driver from "./db.js";
 import { router, setDbReady } from "./routes.js";
-import generateData from "./generateData";
+import generateData from "./generateData.js";
 
 const app = express();
 const PORT = 3000;
--- src/generateData.js
+++ src/generateData.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url"; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const isFileEmpty = (filePath) => {
   const content = fs.readFileSync(filePath, 'utf8');
@@ -152,5 +156,4 @@ const generateData = (brigadesCount, operatorsCount, adminsCount, callFormsCount
   console.log("Генерация данных завершена!");
 };
 
-module.exports = generateData;
-
+export default generateData;
