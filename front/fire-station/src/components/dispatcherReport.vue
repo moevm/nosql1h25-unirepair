4161cd0273d7d2dfd647dcfb6119e4f748a48f37
Надя <nadya@MacBook-Pro-Nada.local>
2025-04-11 14:18:31 +0300

    add DispatcherReportsView

---
+++ front/fire-station/src/components/dispatcherReport.vue
@@ -0,0 +1,95 @@
+<template xmlns="http://www.w3.org/1999/html">
+  <div class="report">
+    <button class="close-btn" @click="$emit('close')">✖</button>
+    <h2 class="report__title">Отчет №{{ reportData.number }}</h2>
+
+    <section class="report__block report__block__border">
+      <div><b>Бригадир:</b> {{ reportData.brigadier }}</div>
+      <div><b>Оператор:</b> {{ reportData.operator }}</div>
+      <div><b>Время вызова:</b> {{ reportData.callTime }}</div>
+      <div><b>Вызов завершен:</b> {{ reportData.endTime }}</div>
+    </section>
+    <section class="report__block">
+      <div><b>Адрес:</b> {{ reportData.address }}</div>
+      <div><b>Категория:</b> {{ reportData.category }}</div>
+      <div><b>Пострадавшие:</b> {{ reportData.victims }}</div>
+
+      <div>
+        <b>Бригада и техника:</b>
+        <ul>
+          <li v-for="(item, index) in reportData.team" :key="index">{{ item }}</li>
+        </ul>
+      </div>
+<!--      карта будет позже-->
+      <div class="map-container">
+        <div class="map-title">Место</div>
+        <img class="map-image" src='../../public/map.png'>
+      </div>
+    </section>
+  </div>
+</template>
+<script setup>
+  defineProps({
+  reportData: Object
+})
+
+</script>
+
+<style scoped>
+
+.map-container {
+  height: 350px;
+}
+
+.map-title {
+  font-weight: bold;
+}
+
+.map-image {
+
+  width: 30%;
+  max-width: 600px;
+  margin: 0 auto;
+  padding-top: 30px;
+  padding-left: 30px;
+}
+
+.report {
+  overflow: hidden;
+  position: relative;
+  box-sizing: border-box;
+  margin: 24px;
+  min-width: 70vw;
+  width: 100%;
+  background: #fff;
+  padding: 24px;
+  border-radius: 12px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+.report__title {
+  font-size: 20px;
+  display: flex;
+  gap: 10px;
+  padding: 0 20px 10px;
+  margin: 0 -24px 20px;
+  border-bottom: #ced0e9 4px solid;
+  justify-content: center;
+}
+.report__block {
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+}
+.report__block__border {
+  border-bottom: black 2px solid;
+}
+.close-btn {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border: none;
+  background-color: #fff;
+  cursor: pointer;
+  color: #900b09;
+  font-size: 24px;
+}
+</style>
---
+++ front/fire-station/src/views/DispatcherReportsView.vue
@@ -0,0 +1,56 @@
+
+<template>
+  <div class="layout">
+    <Sidebar/>
+    <template v-if="showReport">
+      <Report class="report" @close="closeReport" :reportData="currentReport" />
+    </template>
+    <template v-else>
+      <dispatcherReports @openReport="openReport"/>
+    </template>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Sidebar from "@/components/Sidebar.vue";
+import dispatcherReports from "@/components/dispatcherReports.vue";
+import Report from "@/components/dispatcherReport.vue";
+
+export default {
+  name: 'DispatcherReportsView',
+  components: { Sidebar, dispatcherReports, Report },
+  setup() {
+    const showReport = ref(false)
+    const currentReport = ref(null)
+
+    const openReport = (reportData) => {
+      currentReport.value = reportData
+      showReport.value = true
+    }
+
+    const closeReport = () => {
+      showReport.value = false
+      currentReport.value = null
+    }
+
+    return {
+      showReport,
+      currentReport,
+      openReport,
+      closeReport
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.layout {
+  display: flex;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  background-color: #CED0E9;
+}
+</style>
