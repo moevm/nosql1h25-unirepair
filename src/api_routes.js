7eec6f1ebbe55342689a026a5a28798cd83889fc
CoreJust <egor2105094ilin@gmail.com>
2025-04-12 17:39:11 +0300

    Implemented API routes

--- src/api_routes.js
+++ src/api_routes.js
@@ -1,91 +1,166 @@
-import * as query from "./query.js";
-import crypto from "crypto";
+import { create, match, props } from "./query.js";
+
+function fishOut(obj, prop) {
+  const result = obj[prop];
+  delete obj[prop];
+  return result;
+}
+
+function removeNulls(obj) {
+  for (const k of Object.keys(obj)) {
+    if (obj[k] === null) delete obj[k];
+  }
+  return obj;
+}
 
 const api_routes = {
-  "brigade_reports/brigadeNumber:uint": async ({ brigadeNumber }) => {
-    return {
-      complete_reports: await query.match(
-        `(u:User:Brigadier${query.props({ brigadeNumber })})-[:FILLED_IN]->(r:Report:Complete)-[:ON_CALL]->(cf:CallForm:Complete)`,
-        ["r", "cf"],
-        { orderBy: "cf.createdAt DESC" },
-      ),
-      incomplete_reports: await query.match(
-        `(u:User:Brigadier${query.props({ brigadeNumber })})-[:FILLED_IN]->(r:Report:Incomplete)-[:ON_CALL]->(cf:CallForm:Complete)`,
-        ["r", "cf"],
-        { orderBy: "cf.createdAt DESC" },
-      ),
-    };
-  },
-  "find_user/familyName:string? firstName:string? fatherName:string? role:string? brigadeNumber:uint?":
-    async ({ familyName, firstName, fatherName, role, brigadeNumber }) => {
-      return await query.match(
-        `(u:User${query.props({ familyName, firstName, fatherName, brigadeNumber }, [role])})`,
-        ["u"],
-        { orderBy: "u.name DESC" },
-      );
-    },
-   "get_brigades": async () => {
-    const brigades = await query.match(
-      `(b:User:Brigadier)`,
-      ["b.brigadeNumber"]
-    );
-
-    if (!brigades.length) {
-      return { message: "Бригады отсутствуют." };
+  // 1. All information on user from their login and password
+  "login_user/login password:password": async ({ login, password }) => {
+    const users = await match(`(u:User${props({ login })})`, {
+      results: ["u"],
+    });
+    if (!users.length) {
+      return { error: "User does not exist" };
     }
-    const callForms = await query.match(
-      `(cf:CallForm)`,
-      ["cf.status", "cf.assignedTo", "cf.createdAt", "cf.modifiedAt"],
-      { orderBy: "cf.createdAt DESC" }
-    );
-
-    let freeBrigades = [];
-    let busyBrigades = [];
-
-    for (const { brigadeNumber } of brigades) {
-      const brigadeCalls = callForms.filter((cf) => cf.assignedTo.includes(brigadeNumber));
-      if (brigadeCalls.length === 0) {
-        freeBrigades.push({ brigadeNumber, lastCallEndedAt: null });
-      } else {
-        const lastCall = brigadeCalls[0];
-
-        if (lastCall.status === "Complete") {
-          freeBrigades.push({ brigadeNumber, lastCallEndedAt: lastCall.modifiedAt });
-        } else {
-          busyBrigades.push({ brigadeNumber, activeCallStartedAt: lastCall.createdAt });
-        }
-      }
+    const user = users[0];
+    if (user.passwordHash !== password.hash) {
+      return { error: "Password is incorrect" };
     }
-    freeBrigades.sort((a, b) => (a.lastCallEndedAt || 0) - (b.lastCallEndedAt || 0));
-    busyBrigades.sort((a, b) => b.activeCallStartedAt - a.activeCallStartedAt);
-
-    return [...freeBrigades, ...busyBrigades];
+    return user;
   },
-  "get_calls/brigadeNumber:uint": async ({ brigadeNumber }) => {
-    const callForms = await query.match(
-      `(cf:CallForm) WHERE $brigadeNumber IN cf.assignedTo`, 
-      ["cf"],
-      { orderBy: "cf.createdAt DESC" }
+  // 2. Current call forms on a brigade
+  "get_callforms/brigadeNumber:uint": async ({ brigadeNumber }) => {
+    const callForms = await match(
+      `(cf:CallForm) WHERE $brigadeNumber IN cf.assignedTo`,
+      { results: ["cf"], orderBy: "cf.createdAt DESC" },
     );
     if (!callForms.length) {
       return { message: "No active calls" };
     }
     return callForms;
   },
-  "login_user/login:string password:string": async ({ login, password }) => {
-    const hashedPassword =  crypto.createHash("sha256").update(password).digest("hex");
-    const users = await query.match(
-      `(u:User${query.props({ login })})`, 
-      ["u"]
+  // 3. Reports on a brigade
+  "brigade_reports/brigadeNumber:uint": async ({ brigadeNumber }) => {
+    return {
+      complete_reports: await match(
+        `(u:User:Brigadier${props({ brigadeNumber })})-[:FILLED_IN]->(r:Report:Complete)-[:ON_CALL]->(cf:CallForm:Complete)`,
+        { results: ["r", "cf"], orderBy: "cf.createdAt DESC" },
+      ),
+      incomplete_reports: await match(
+        `(u:User:Brigadier${props({ brigadeNumber })})-[:FILLED_IN]->(r:Report:Incomplete)-[:ON_CALL]->(cf:CallForm:Complete)`,
+        { results: ["r", "cf"], orderBy: "cf.createdAt DESC" },
+      ),
+    };
+  },
+  // 4. Fill in a report
+  "fill_report/callform:uint": async ({ callform }) => {},
+  // 5. Create a new callform
+  "create_callform/callSource:string? fireAddress:string? bottomLeft:point? topRight:point? fireType:string? fireRank:string? victimsCount:uint? assignedTo:uint? auto:string?":
+    async (args) => {
+      if (Object.values(args).includes(null))
+        return { error: "Incomplete callforms are not supported yet" };
+      await create(
+        props(
+          { ...args, createdAt: "timestamp()", modifiedAt: "timestamp()" },
+          ["CallForm", "Complete"],
+        ),
+      );
+      return {};
+    },
+  // 6. Find operator's complete callforms
+  "operator_callforms/login": async ({ login }) => {
+    return await match(
+      `(u:User:Operator${props({ login })})-[:CREATED]->(cf:CallForm:Complete)`,
+      { results: ["cf"], orderBy: "cf.createdAt DESC" },
     );
-    if (!users.length) {
-      return { error: "User does not exist" };
+  },
+  // 7. Spawn user
+  "user_spawn/familyName firstName fatherName role brigadeNumber:uint address phone email login password:password":
+    async (args) => {
+      const role = fishOut(args, "role");
+      const labels = ["User", role].concat(
+        role === "Brigadier" ? ["Fireman"] : [],
+      );
+      await create(
+        props(
+          { ...args, registeredAt: "timestamp()", modifiedAt: "timestamp()" },
+          labels,
+        ),
+      );
+      return {};
+    },
+  // 8. User search
+  "user_search/familyName? firstName? fatherName? role? brigadeNumber:uint?":
+    async ({ familyName, firstName, fatherName, role, brigadeNumber }) => {
+      return await match(
+        `(u:User${props({ familyName, firstName, fatherName, brigadeNumber }, [role])})`,
+        { results: ["u"], orderBy: "u.name DESC" },
+      );
+    },
+  // 9. User modification
+  "modify_user/familyName? firstName? fatherName? role? brigadeNumber:uint? address? phone? email? login password:password?":
+    async (args) => {
+      args = removeNulls(args);
+      if (Object.keys(args).length === 1) return {};
+      const role = fishOut(args, "role");
+      const labels = role
+        ? [role].concat(role === "Brigadier" ? ["Fireman"] : [])
+        : undefined;
+      const login = fishOut(args, "login");
+      const remove = labels
+        ? { u: ["Operator", "Administrator", "Fireman", "Brigidier"] }
+        : undefined;
+      await match(`(u:User${props({ login })})`, {
+        remove,
+        set: { u: { labels, props: { ...args, modifiedAt: "timestamp()" } } },
+      });
+      return {};
+    },
+  // 10. Get currently free and busy brigades
+  get_brigades: async () => {
+    const brigades = await match(`(b:User:Brigadier)`, {
+      results: ["b.brigadeNumber"],
+    });
+
+    if (!brigades.length) {
+      return { message: "Бригады отсутствуют." };
     }
-    const user = users[0];
-    if (user.passwordHash !== hashedPassword) {
-      return { error: "Password is incorrect" };
+    const callForms = await match(`(cf:CallForm)`, {
+      results: ["cf.status", "cf.assignedTo", "cf.createdAt", "cf.modifiedAt"],
+      orderBy: "cf.createdAt DESC",
+    });
+
+    let freeBrigades = [];
+    let busyBrigades = [];
+
+    for (const { brigadeNumber } of brigades) {
+      const brigadeCalls = callForms.filter((cf) =>
+        cf.assignedTo.includes(brigadeNumber),
+      );
+      if (brigadeCalls.length === 0) {
+        freeBrigades.push({ brigadeNumber, lastCallEndedAt: null });
+      } else {
+        const lastCall = brigadeCalls[0];
+
+        if (lastCall.status === "Complete") {
+          freeBrigades.push({
+            brigadeNumber,
+            lastCallEndedAt: lastCall.modifiedAt,
+          });
+        } else {
+          busyBrigades.push({
+            brigadeNumber,
+            activeCallStartedAt: lastCall.createdAt,
+          });
+        }
+      }
     }
-    return user;
-  }
+    freeBrigades.sort(
+      (a, b) => (a.lastCallEndedAt || 0) - (b.lastCallEndedAt || 0),
+    );
+    busyBrigades.sort((a, b) => b.activeCallStartedAt - a.activeCallStartedAt);
+
+    return [...freeBrigades, ...busyBrigades];
+  },
 };
 export default api_routes;
--- src/query.js
+++ src/query.js
@@ -1,34 +1,11 @@
 import * as assert from "./assert.js";
 import driver from "./db.js";
 
-export async function match(conditions, results, options = {}) {
-  assert.assertString(conditions);
-  assert.assertObject(options);
-  if (options.orderBy) assert.assertString(options.orderBy);
-  if (options.limit) assert.assertString(options.limit);
-  const session = driver.session();
-  try {
-    const query = `
-      MATCH ${conditions}
-      RETURN ${results.join(", ")}
-      ${options.orderBy ? "ORDER BY " + options.orderBy : ""}
-      ${options.limit ? "LIMIT " + options.limit : ""};
-    `;
-    return await session
-      .executeRead((tx) => tx.run(query))
-      .then((result) =>
-        result.records.map((record) => {
-          let records = [];
-          for (const r of results) records.push(record.get(r));
-          return records;
-        }),
-      )
-      .catch((err) => {
-        throw err;
-      });
-  } finally {
-    await session.close();
-  }
+function makeNeo4jLiteral(value) {
+  if (value && value.longitude && value.latitude)
+    return `point({latitude: ${value.latitude}, longitude: ${value.longitude}})`;
+  if (value && value.password && value.hash) return value.hash;
+  return value;
 }
 
 export function props(conditions, labels = []) {
@@ -42,8 +19,100 @@ export function props(conditions, labels = []) {
     " { " +
     Object.entries(conditions)
       .filter(([key, cond]) => key !== null && cond !== null)
-      .map(([key, cond]) => `${key}: "${cond}"`)
+      .map(([key, cond]) => `${key}: "${makeNeo4jLiteral(cond)}"`)
       .join(", ") +
     " }"
   );
+}
+
+export async function create(value) {
+  assert.assertString(value);
+  const session = driver.session();
+  try {
+    const query = `CREATE (${conditions});`;
+    await session.run(query).catch((err) => {
+      throw err;
+    });
+  } finally {
+    await session.close();
+  }
+}
+
+export async function match(conditions, options = {}) {
+  assert.assertString(conditions);
+  assert.assertObject(options);
+  if (options.where) assert.assertString(options.where);
+  if (options.remove) {
+    assert.assertObject(options.remove);
+    for (const [k, v] of Object.entries(options.remove)) assert.assertArray(v);
+  }
+  if (options.set) {
+    assert.assertObject(options.set);
+    for (const [k, v] of Object.entries(options.set)) {
+      assert.assert(v && (v.labels || v.props));
+      if (v.labels) assert.assertArray(v.labels);
+      if (v.props) assert.assertObject(v.props);
+    }
+  }
+  if (options.results) assert.assertArray(options.results);
+  if (options.orderBy) assert.assertString(options.orderBy);
+  if (options.limit) assert.assertString(options.limit);
+
+  const session = driver.session();
+  let result = null;
+  try {
+    const query = `
+      MATCH ${conditions}
+      ${options.where ? "WHERE " + options.where : ""}
+      ${
+        options.remove
+          ? "REMOVE " +
+            Object.entries(options.remove)
+              .map(([k, v]) => v.map((x) => `${k} :${x}`).join(",\n\t"))
+              .join(",\n\t")
+          : ""
+      }
+      ${
+        options.set
+          ? "SET " +
+            Object.entries(options.set)
+              .map(
+                ([k, v]) =>
+                  (v.labels
+                    ? v.labels.map((x) => `${k} :${x}`).join(",\n\t")
+                    : "") +
+                  (v.props
+                    ? Object.entries(v.props)
+                        .map(
+                          ([field, value]) =>
+                            `${k}.${field} = ${makeNeo4jLiteral(value)}`,
+                        )
+                        .join(",\n\t")
+                    : ""),
+              )
+              .join(",\n\t")
+          : ""
+      }
+      ${options.results ? "RETURN [" + options.results.map((r) => `"r"`).join(", ") + "]" : ""}
+      ${options.orderBy ? "ORDER BY " + options.orderBy : ""}
+      ${options.limit ? "LIMIT " + options.limit : ""};
+    `;
+    result = await session
+      .executeRead((tx) => tx.run(query))
+      .then((result) =>
+        options.results
+          ? result.records.map((record) => {
+              let records = [];
+              for (const r of options.results) records.push(record.get(r));
+              return records;
+            })
+          : [],
+      )
+      .catch((err) => {
+        throw err;
+      });
+  } finally {
+    await session.close();
+    return result;
+  }
 }
