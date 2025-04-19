b118a6a6992b5f7a17fa95e4fedb25f7d9a96b03
CoreJust <egor2105094ilin@gmail.com>
2025-04-19 23:41:13 +0300

    Rewritten a part of query handling

--- src/api_routes.js
+++ src/api_routes.js
@@ -1,155 +1,127 @@
-import { create, match, props, byId, now, matches } from "./query.js";
-
-function fishOut(obj, prop) {
-  const result = obj[prop];
-  delete obj[prop];
-  return result;
-}
-
-function fishOutDateRangesAndStrings(obj) {
-  let result = {};
-  for (const k in Object.keys(obj)) {
-    if (typeof obj[k] === "string" || (obj[k] && obj[k].from && obj[k].to)) {
-      result[k] = obj[k];
-      delete obj[k];
-    }
-  }
-  return result;
-}
-
-function removeNulls(obj) {
-  for (const k of Object.keys(obj)) {
-    if (obj[k] === null) delete obj[k];
-  }
-  return obj;
-}
+import {
+  create,
+  match,
+  rawMatch,
+  props,
+  now,
+  fishOutTypes,
+  rawQuery,
+  matches,
+  fishOutComplexConds,
+} from "./query.js";
 
 const api_routes = {
   // 1. All information on user from their login and password
-  "login_user/login password:password": async ({ login, password }) => {
-    const users = await match(`(u:User${props({ login })})`, {
-      results: ["u"],
-    });
+  "login_user/login:string password:password": async ({ login, password }) => {
+    const users = await match("u:User", { login });
     if (!users || users.length === 0) return { error: "User does not exist" };
     const user = users[0];
-    if (user.passwordHash !== password.hash)
+    if (user.passwordHash !== password.value.hash)
       return { error: "Password is incorrect" };
     return user;
   },
   // 2. Current call forms on a brigade
-  "get_callforms/brigadeNumber:uint": async ({ brigadeNumber }) => {
-    const callForms = await match(
-      `(cf:CallForm${props({ assignedTo: brigadeNumber })})`,
-      {
-        results: ["cf"],
-        orderBy: "cf.createdAt DESC",
-      },
-    );
+  "get_callforms/assignedTo:uint": async (args) => {
+    const callForms = await match("cf:CallForm", args, {
+      orderBy: "cf.createdAt DESC",
+    });
     if (!callForms || callForms.length === 0)
       return { message: "No active calls" };
     return callForms;
   },
   // 3. Reports on a brigade
-  "brigade_reports/brigadeNumber:uint": async ({ brigadeNumber }) => {
+  "brigade_reports/brigadeNumber:uint": async (args) => {
     return {
-      complete_reports: await match(
-        `(u:User:Brigadier${props({ brigadeNumber })})-[:FILLED_IN]->(r:Report:Complete)-[:ON_CALL]->(cf:CallForm:Complete)`,
+      complete_reports: await rawMatch(
+        `(u:User:Brigadier${props(args)})-[:FILLED_IN]->(r:Report:Complete)-[:ON_CALL]->(cf:CallForm:Complete)`,
         { results: ["r", "cf"], orderBy: "cf.createdAt DESC" },
       ),
-      incomplete_reports: await match(
-        `(u:User:Brigadier${props({ brigadeNumber })})-[:FILLED_IN]->(r:Report:Incomplete)-[:ON_CALL]->(cf:CallForm:Complete)`,
+      incomplete_reports: await rawMatch(
+        `(u:User:Brigadier${props(args)})-[:FILLED_IN]->(r:Report:Incomplete)-[:ON_CALL]->(cf:CallForm:Complete)`,
         { results: ["r", "cf"], orderBy: "cf.createdAt DESC" },
       ),
-      new_reports: await match(
-        `(u:User:Brigadier${props({ brigadeNumber })})-[:FILLED_IN]->(r:Report:New)-[:ON_CALL]->(cf:CallForm:Complete)`,
+      new_reports: await rawMatch(
+        `(u:User:Brigadier${props(args)})-[:FILLED_IN]->(r:Report:New)-[:ON_CALL]->(cf:CallForm:Complete)`,
         { results: ["r", "cf"], orderBy: "cf.createdAt DESC" },
       ),
     };
   },
   // 4. Fill in a report
-  "fill_report/reportId:uint waterSpent:uint foamSpent:uint allegedFireCause damage:uint additionalNotes":
+  "fill_report/reportId:id waterSpent:uint foamSpent:uint allegedFireCause damage:uint additionalNotes":
     async (args) => {
-      const reportId = fishOut(args, "reportId");
-      await match(`(r:Report:New)`, {
-        where: byId("r", reportId),
+      await match("(r:Report:New)", fishOutTypes(args, ["id"]), {
         remove: { r: ["New"] },
         set: { r: { labels: ["Complete"], props: args } },
       });
       return {};
     },
   // 5. Create a new callform
-  "create_callform/callSource:string? fireAddress:string? bottomLeft:point? topRight:point? fireType:string? fireRank:string? victimsCount:uint? assignedTo:uint? auto:string?":
+  "create_callform/callSource? fireAddress? bottomLeft:point? topRight:point? fireType? fireRank:string? victimsCount:uint? assignedTo:uint? auto?":
     async (args) => {
       if (Object.values(args).includes(null))
         return { error: "Incomplete callforms are not supported yet" };
-      await create(
-        props({ ...args, createdAt: now(), modifiedAt: now() }, [
-          "CallForm",
-          "Complete",
-        ]),
-      );
-      return {};
+      return await create(":CallForm:Complete", {
+        ...args,
+        createdAt: now(),
+        modifiedAt: now(),
+      });
     },
   // 6. Find operator's complete callforms
-  "operator_callforms/login": async ({ login }) => {
-    return await match(
-      `(u:User:Operator${props({ login })})-[:CREATED]->(cf:CallForm:Complete)`,
+  "operator_callforms/login:string": async (args) => {
+    return await rawMatch(
+      `(u:User:Operator${props(args)})-[:CREATED]->(cf:CallForm:Complete)`,
       { results: ["cf"], orderBy: "cf.createdAt DESC" },
     );
   },
   // 7. Spawn user
-  "user_spawn/familyName firstName fatherName role brigadeNumber:uint address phone email login password:password":
+  "user_spawn/familyName firstName fatherName role:label brigadeNumber:uint address phone email login:string password:password":
     async (args) => {
-      const role = fishOut(args, "role");
-      const labels = ["User", "Active", role].concat(
-        role === "Brigadier" ? ["Fireman"] : [],
+      return await create(
+        ":User:Active" + (args.role === "Brigadier" ? ":Fireman" : ""),
+        {
+          ...args,
+          registeredAt: now(),
+          modifiedAt: now(),
+        },
       );
-      await create(
-        props({ ...args, registeredAt: now(), modifiedAt: now() }, labels),
-      );
-      return {};
     },
   // 8. User search
-  "user_search/familyName? firstName? fatherName? role? brigadeNumber:uint? address? phone? email? registeredAt:daterange? modifiedAt:daterange?":
+  "user_search/familyName? firstName? fatherName? role:label? brigadeNumber:uint? address? phone? email? registeredAt:daterange? modifiedAt:daterange?":
     async (args) => {
-      const role = fishOut(args, "role");
-      const brigadeNumber = fishOut(args, "brigadeNumber");
-      return await match(`(u:User${props({ brigadeNumber }, [role])})`, {
-        where: matches("u", args),
-        results: ["u"],
-        orderBy: "u.name DESC",
-      });
+      return await match("u:User", args, { orderBy: "u.name DESC" });
     },
   // 9. User modification
-  "modify_user/familyName? firstName? fatherName? role? brigadeNumber:uint? address? phone? email? login password:password?":
+  "modify_user/familyName? firstName? fatherName? role:label? brigadeNumber:uint? address? phone? email? login:string password:password?":
     async (args) => {
-      args = removeNulls(args);
       if (Object.keys(args).length === 1) return {};
-      const role = fishOut(args, "role");
-      const labels = role
-        ? [role].concat(role === "Brigadier" ? ["Fireman"] : [])
-        : undefined;
-      const login = fishOut(args, "login");
-      const remove = labels
-        ? { u: ["Operator", "Administrator", "Fireman", "Brigidier"] }
-        : undefined;
-      await match(`(u:User${props({ login })})`, {
-        remove,
-        set: { u: { labels, props: { ...args, modifiedAt: now() } } },
-      });
+      const login = fishOutTypes(args, ["string"]).login;
+      await match(
+        "u:User",
+        { login },
+        {
+          remove: args.role
+            ? { u: ["Operator", "Administrator", "Fireman", "Brigidier"] }
+            : undefined,
+          set: { u: { ...args, modifiedAt: now() } },
+        },
+      );
       return {};
     },
   // 10. Get currently free and busy brigades
   get_brigades: async () => {
-    let activeCalls = await match("(cf:CallForm:Incomplete)", {
-      results: ["COLLECT(DISTINCT cf.assignedTo) AS busybodies"],
-    });
+    let activeCalls = await match(
+      "cf:CallForm:Incomplete",
+      {},
+      {
+        results: ["COLLECT(DISTINCT cf.assignedTo) AS busybodies"],
+      },
+    );
     activeCalls =
       activeCalls && activeCalls.length
         ? `[ ${activeCalls[0].filter((x) => x !== 0).join(", ")} ]`
         : "[]";
     return {
-      busyBrigades: await match(
+      busyBrigades: await rawMatch(
         `(u:User:Brigadier)
         WHERE u.brigadeNumber IN ${activeCalls}
         MATCH (brigadeCf:CallForm:Incomplete { assignedTo: u.brigadeNumber })
@@ -162,7 +134,7 @@ const api_routes = {
           orderBy: "activeCallStartedAt DESC",
         },
       ),
-      freeBrigades: await match(
+      freeBrigades: await rawMatch(
         `(u:User:Brigadier)
         WHERE NOT u.brigadeNumber IN ${activeCalls} AND u.brigadeNumber <> 0
         OPTIONAL MATCH (brigadeCf:CallForm:Complete { assignedTo: u.brigadeNumber })
@@ -178,70 +150,55 @@ const api_routes = {
     };
   },
   // 11. Create a new report based on complete callform
-  "new_report/callformId:uint": async ({ callformId }) => {
-    await match(`(cf:CallForm:Complete)`, {
-      where: byId("cf", callformId),
-      create: `(r:Report:New${props({
+  "new_report/callformId:id": async (args) => {
+    await match("cf:CallForm:Complete", args, {
+      create: `(r:Report:New {
         waterSpent: 0,
         foamSpent: 0,
         allegedFireCause: "неизвестно",
         damage: 0,
         additionalNotes: "Данные ещё не внесены, ожидается завершение отчёта.",
-        modifiedAt: now(),
-      })})\nCREATE (r)-[:ON_CALL]->(cf)`,
+        modifiedAt: datetime()
+      })\nCREATE (r)-[:ON_CALL]->(cf)`,
     });
     return {};
   },
-  "test_query/query": async ({ query }) => {
-    const result = await match(query, { results: ["u"] });
-    console.log(`Query: ${query};\nResult: ${JSON.stringify(result)}`);
+  "test_query/query:string": async ({ query }) => {
+    const result = await rawQuery(query.value);
+    console.log(`Query: ${query.value};\nResult: ${JSON.stringify(result)}`);
     return result;
   },
   // Сall forms search
-  "callform_search/status? createdAt:daterange? modifiedAt:daterange? callSource? fireAddress? fireType? fireRank? victimsCount:uint? assignedTo:uint?":
+  "callform_search/status:label? createdAt:daterange? modifiedAt:daterange? callSource? fireAddress? fireType? fireRank? victimsCount:uint? assignedTo:uint?":
     async (args) => {
-      const status = fishOut(args, "status");
-      const matchArgs = fishOutDateRangesAndStrings(args);
-      return await match(`(cf:CallForm${props(args, [status])})`, {
-        where: matches("cf", matchArgs),
-        results: ["cf"],
+      return await match("cf:CallForm", args, {
         orderBy: "cf.createdAt DESC",
       });
     },
   // Report search
-  "report_search/status? modifiedAt:daterange? waterSpent:uint? foamSpent:uint? allegedFireCause? damage:uint? additionalNotes?":
+  "report_search/status:label? modifiedAt:daterange? waterSpent:uint? foamSpent:uint? allegedFireCause? damage:uint? additionalNotes?":
     async (args) => {
-      const status = fishOut(args, "status");
-      const matchArgs = fishOutDateRangesAndStrings(args);
-      return await match(`(r:Report${props(args, [status])})`, {
-        where: matches("r", matchArgs),
-        results: ["r"],
+      return await match("r:Report", args, {
         orderBy: "r.modifiedAt DESC",
       });
     },
   // Inventory search
   "inventory_search/name?": async (args) => {
-    return await match(`(i:Inventory)`, {
-      where: matches("i", args),
-      results: ["i"],
-      orderBy: "i.name ASC",
-    });
+    return await match("i:Inventory", args, { orderBy: "i.name ASC" });
   },
   // Find a report by author
-  "report_search_by_author/familyName? firstName? fatherName? role? brigadeNumber:uint? callFormCreatedAt:daterange? modifiedAt:daterange?":
-  async (args) => {
-    const role = fishOut(args, "role");
-    const brigadeNumber = fishOut(args, "brigadeNumber");
-    const matchArgs = fishOutDateRangesAndStrings(args);
-
-    return await match(
-      `(u:User${props({ brigadeNumber }, [role])})-[:FILLED_IN]->(r:Report)-[:ON_CALL]->(cf:CallForm)`,
-      {
-        where: matches("r", matchArgs), 
-        results: ["r", "u", "cf"],
-        orderBy: "r.modifiedAt DESC",
-      }
-    );
-  },
+  "report_search_by_author/familyName? firstName? fatherName? role:label? brigadeNumber:uint? createdAt:daterange? modifiedAt:daterange?":
+    async (args) => {
+      const matchArgs = fishOutComplexConds(args);
+      const rArgs = fishOutTypes(matchArgs, ["daterange"]);
+      return await rawMatch(
+        `(u:User${props(args)})-[:FILLED_IN]->(r:Report)-[:ON_CALL]->(cf:CallForm)`,
+        {
+          where: matches({ r: rArgs, u: matchArgs }),
+          results: ["r", "u", "cf"],
+          orderBy: "r.modifiedAt DESC",
+        },
+      );
+    },
 };
 export default api_routes;
