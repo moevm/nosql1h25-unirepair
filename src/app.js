72a412beff2c6921683f68a3a330b286568990b5
EfremovaAnna <efremova.anna.vyacheslavovna@gmail.com>
2025-04-20 21:42:44 +0300

    fix path

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors({
-  origin: 'http://127.0.0.1:5173',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
