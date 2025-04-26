3ef4f589d5d2f295a9402262db3956e64bff73be
efremova_anna <efremova.anna.vyacheslavovna@gmail.com>
2025-04-27 00:53:13 +0300

    fix tables

--- front/fire-station/src/components/fireReport.vue
+++ front/fire-station/src/components/fireReport.vue
@@ -85,85 +85,89 @@
 
             <div class="form-group">
                 <label>Выбор бригад:</label>
-                <table class="selection-table">
-                    <thead>
-                        <tr>
-                            <th>Выбрать</th>
-                            <th>Номер бр.</th>
-                            <th>Размер бр.</th>
-                            <th>Время последнего вызова</th>
-                            <th>Кол-во вызовов за смену</th>
-                            <th>Статус</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr
-                            v-for="(brigade, index) in availableBrigades"
-                            :key="index"
-                        >
-                            <td>
-                                <input
-                                    type="radio"
-                                    v-model="selectedBrigade"
-                                    :value="brigade.number"
-                                />
-                            </td>
-                            <td>{{ brigade.number }}</td>
-                            <td>{{ brigade.size }}</td>
-                            <td>{{ brigade.lastCallTime }}</td>
-                            <td>{{ brigade.callsCount }}</td>
-                            <td
-                                :class="{
-                                    'status-available':
-                                        brigade.status === 'Свободна',
-                                    'status-busy':
-                                        brigade.status === 'На вызове',
-                                }"
+                <div class="table-container">
+                    <table class="selection-table">
+                        <thead>
+                            <tr>
+                                <th>Выбрать</th>
+                                <th>Номер бр.</th>
+                                <th>Размер бр.</th>
+                                <th>Время последнего вызова</th>
+                                <th>Кол-во вызовов за смену</th>
+                                <th>Статус</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                                v-for="(brigade, index) in availableBrigades"
+                                :key="index"
                             >
-                                {{ brigade.status }}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                                <td>
+                                    <input
+                                        type="radio"
+                                        v-model="selectedBrigade"
+                                        :value="brigade.number"
+                                    />
+                                </td>
+                                <td>{{ brigade.number }}</td>
+                                <td>{{ brigade.size }}</td>
+                                <td>{{ brigade.lastCallTime }}</td>
+                                <td>{{ brigade.callsCount }}</td>
+                                <td
+                                    :class="{
+                                        'status-available':
+                                            brigade.status === 'Свободна',
+                                        'status-busy':
+                                            brigade.status === 'На вызове',
+                                    }"
+                                >
+                                    {{ brigade.status }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
             <div class="form-group">
                 <label>Выбор пожарной машины:</label>
-                <table class="selection-table">
-                    <thead>
-                        <tr>
-                            <th>Выбрать</th>
-                            <th>Тип машины</th>
-                            <th>Статус</th>
-                            <th>Номер машины</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr
-                            v-for="(vehicle, index) in availableVehicles"
-                            :key="index"
-                        >
-                            <td>
-                                <input
-                                    type="radio"
-                                    v-model="selectedVehicle"
-                                    :value="vehicle.number"
-                                />
-                            </td>
-                            <td>{{ vehicle.type }}</td>
-                            <td
-                                :class="{
-                                    'status-available':
-                                        vehicle.status === 'Свободна',
-                                    'status-busy': vehicle.status === 'Занята',
-                                }"
+                <div class="table-container">
+                    <table class="selection-table">
+                        <thead>
+                            <tr>
+                                <th>Выбрать</th>
+                                <th>Тип машины</th>
+                                <th>Статус</th>
+                                <th>Номер машины</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                                v-for="(vehicle, index) in availableVehicles"
+                                :key="index"
                             >
-                                {{ vehicle.status }}
-                            </td>
-                            <td>{{ vehicle.number }}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                                <td>
+                                    <input
+                                        type="radio"
+                                        v-model="selectedVehicle"
+                                        :value="vehicle.number"
+                                    />
+                                </td>
+                                <td>{{ vehicle.type }}</td>
+                                <td
+                                    :class="{
+                                        'status-available':
+                                            vehicle.status === 'Свободна',
+                                        'status-busy': vehicle.status === 'Занята',
+                                    }"
+                                >
+                                    {{ vehicle.status }}
+                                </td>
+                                <td>{{ vehicle.number }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
             <div class="form-actions">
@@ -623,5 +627,43 @@ img {
 }
 .confirm-button:hover {
     background-color: #8d5151;
+}
+
+/* добавим прокрутку */
+.table-container {
+  max-height: 250px; /* Высота примерно для 5 строк */
+  overflow-y: auto; /* Вертикальная прокрутка */
+  border: 1px solid #ddd;
+  margin-bottom: 20px;
+  position: relative;
+}
+
+.table-container table {
+  width: 100%;
+}
+
+.table-container thead th {
+  position: sticky;
+  top: 0;
+  background-color: #f2f2f2;
+  z-index: 10;
+}
+
+/* Кастомный скроллбар */
+.table-container::-webkit-scrollbar {
+  width: 8px;
+}
+
+.table-container::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+.table-container::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 4px;
+}
+
+.table-container::-webkit-scrollbar-thumb:hover {
+  background: #555;
 }
 </style>
