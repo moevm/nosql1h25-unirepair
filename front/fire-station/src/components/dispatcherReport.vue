b504bef92b6afddb1d708390c6fc0e29e755a1d5
CoreJust <egor2105094ilin@gmail.com>
2025-04-27 15:07:45 +0300

    Fixed dates display

--- front/fire-station/src/components/dispatcherReport.vue
+++ front/fire-station/src/components/dispatcherReport.vue
@@ -1,119 +1,97 @@
 <template xmlns="http://www.w3.org/1999/html">
-  <div class="report">
-    <button class="close-btn" @click="$emit('close')">✖</button>
-    <h2 class="report__title">Отчет №{{ reportData.number }}</h2>
+	<div class="report">
+		<button class="close-btn" @click="$emit('close')">✖</button>
+		<h2 class="report__title">Отчет №{{ reportData.number }}</h2>
 
-    <section class="report__block report__block__border">
-      <div><b>№ Бригады:</b> {{ reportData.assignedTo }}</div>
-      <div><b>Оператор:</b> {{ operator.fullName}}</div>
-      <div><b>Время вызова:</b> {{ formDate(reportData.createdAt) }}</div>
-      <div><b>Вызов завершен:</b> {{ formDate(reportData.modifiedAt) }}</div>
-    </section>
-    <section class="report__block">
-      <div><b>Адрес:</b> {{ reportData.fireAddress }}</div>
-      <div><b>Категория:</b> {{ reportData.fireRank }}</div>
-      <div><b>Пострадавшие:</b> {{ reportData.victimsCount }}</div>
+		<section class="report__block report__block__border">
+			<div><b>№ Бригады:</b> {{ reportData.assignedTo }}</div>
+			<div><b>Оператор:</b> {{ operator.fullName }}</div>
+			<div><b>Время вызова:</b> {{ reportData.createdAt }}</div>
+			<div><b>Вызов завершен:</b> {{ reportData.modifiedAt }}</div>
+		</section>
+		<section class="report__block">
+			<div><b>Адрес:</b> {{ reportData.fireAddress }}</div>
+			<div><b>Категория:</b> {{ reportData.fireRank }}</div>
+			<div><b>Пострадавшие:</b> {{ reportData.victimsCount }}</div>
 
-      <div>
-        <b>Бригада и техника:</b>
-        <ul>
-          <li>{{ reportData.auto }}</li>
-        </ul>
-      </div>
-<!--      карта будет позже-->
-      <div class="map-container">
-        <div class="map-title">Место</div>
-        <img class="map-image" src='../../public/map.png'>
-      </div>
-    </section>
-  </div>
+			<div>
+				<b>Бригада и техника:</b>
+				<ul>
+					<li>{{ reportData.auto }}</li>
+				</ul>
+			</div>
+			<!--      карта будет позже-->
+			<div class="map-container">
+				<div class="map-title">Место</div>
+				<img class="map-image" src="../../public/map.png" />
+			</div>
+		</section>
+	</div>
 </template>
 
 <script setup>
-import { useUserStore } from '@/stores/user';
+import { useUserStore } from "@/stores/user";
 
 defineProps({
-  reportData: Object
-})
-
-const formDate = (date) => {
-    let day = date.day.toString();
-    while(day.length < 2) day = '0' + day;
-
-    let month = date.month.toString();
-    while(month.length < 2) month = '0' + month;
-
-    let year = date.year.toString();
-    while(year.length < 4) year = '0' + year;
-
-    let hour = date.hour.toString();
-    while(hour.length < 2) hour = '0' + hour
-
-    let minute = date.minute.toString();
-    while(minute.length < 2) minute = '0' + minute
-
-    return `${hour}:${minute}, ${year}-${month}-${day}`
-}
+	reportData: Object,
+});
 
 const operator = useUserStore().user;
-
 </script>
 
 <style scoped>
-
 .map-container {
-  height: 350px;
+	height: 350px;
 }
 
 .map-title {
-  font-weight: bold;
+	font-weight: bold;
 }
 
 .map-image {
-
-  width: 30%;
-  max-width: 600px;
-  margin: 0 auto;
-  padding-top: 30px;
-  padding-left: 30px;
+	width: 30%;
+	max-width: 600px;
+	margin: 0 auto;
+	padding-top: 30px;
+	padding-left: 30px;
 }
 
 .report {
-  overflow: hidden;
-  position: relative;
-  box-sizing: border-box;
-  margin: 24px;
-  min-width: 70vw;
-  width: 100%;
-  background: #fff;
-  padding: 24px;
-  border-radius: 12px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	overflow: hidden;
+	position: relative;
+	box-sizing: border-box;
+	margin: 24px;
+	min-width: 70vw;
+	width: 100%;
+	background: #fff;
+	padding: 24px;
+	border-radius: 12px;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 .report__title {
-  font-size: 20px;
-  display: flex;
-  gap: 10px;
-  padding: 0 20px 10px;
-  margin: 0 -24px 20px;
-  border-bottom: #ced0e9 4px solid;
-  justify-content: center;
+	font-size: 20px;
+	display: flex;
+	gap: 10px;
+	padding: 0 20px 10px;
+	margin: 0 -24px 20px;
+	border-bottom: #ced0e9 4px solid;
+	justify-content: center;
 }
 .report__block {
-  padding-bottom: 10px;
-  margin-bottom: 10px;
+	padding-bottom: 10px;
+	margin-bottom: 10px;
 }
 .report__block__border {
-  border-bottom: black 2px solid;
+	border-bottom: black 2px solid;
 }
 .close-btn {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  border: none;
-  background-color: #fff;
-  cursor: pointer;
-  color: #900b09;
-  font-size: 24px;
+	position: absolute;
+	top: 10px;
+	right: 10px;
+	border: none;
+	background-color: #fff;
+	cursor: pointer;
+	color: #900b09;
+	font-size: 24px;
 }
 </style>
--- front/fire-station/src/views/DispatcherActiveCallView.vue
+++ front/fire-station/src/views/DispatcherActiveCallView.vue
@@ -1,68 +1,61 @@
 <template>
-  <div class="layout">
-    <Sidebar/>
-    <div class="block">
-      <div class="calls__header">
-        <span class="calls__header-title">Текущие вызовы</span>
-      </div>
-        <dispatcherCall
-            v-for="call in calls"
-            :key="call.id"
-            :call="call"
-        />
-        <dispatcherCall
-            v-for="call in calls"
-            :key="call.id"
-            :call="call"
-        />
-    </div>
-  </div>
+	<div class="layout">
+		<Sidebar />
+		<div class="block">
+			<div class="calls__header">
+				<span class="calls__header-title">Текущие вызовы</span>
+			</div>
+			<dispatcherCall v-for="call in calls" :key="call.id" :call="call" />
+			<dispatcherCall v-for="call in calls" :key="call.id" :call="call" />
+		</div>
+	</div>
 </template>
 
 <script setup>
-import axios from 'axios'
-import Sidebar from '../components/Sidebar.vue'
-import dispatcherCall from '../components/dispatcherCall.vue'
-import { useUserStore } from '../stores/user.js'
-import { ref } from 'vue';
+import Sidebar from "../components/Sidebar.vue";
+import dispatcherCall from "../components/dispatcherCall.vue";
+import { useUserStore } from "../stores/user.js";
+import { ref } from "vue";
+import query from "../common/query.js";
 
 let calls = ref([]);
-axios.get(`http://localhost:3000/api/operator_callforms?login=${useUserStore().user.login}`).then(res => calls.value = res.data.incomplete_callforms);
-
+query("operator_callforms", { login: useUserStore().user.login }).then(
+	(res) => (calls.value = res.incomplete_callforms),
+);
 </script>
 
 <style scoped>
 .layout {
-  display: flex;
-  background-color: #CED0E9;
-  min-height: 100vh;
+	display: flex;
+	background-color: #ced0e9;
+	min-height: 100vh;
 }
 .calls__header {
-  display: flex;
-  margin-left: 24px;
-  margin-right: 24px;
-  margin-top: 24px;
-  justify-content: center;
-  align-items: center;
-  background: #fff;
-  padding: 20px;
-  border-radius: 12px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	display: flex;
+	margin-left: 24px;
+	margin-right: 24px;
+	margin-top: 24px;
+	justify-content: center;
+	align-items: center;
+	background: #fff;
+	padding: 20px;
+	border-radius: 12px;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 
-  position: sticky;
-  top: 0;
+	position: sticky;
+	top: 0;
 }
-.calls__header-title{
-  font-weight: bold;
-  font-size: x-large;
+.calls__header-title {
+	font-weight: bold;
+	font-size: x-large;
 }
 
 .block {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  overflow-y: scroll;
-  max-height: 100vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	overflow-y: scroll;
+	max-height: 100vh;
 }
 </style>
