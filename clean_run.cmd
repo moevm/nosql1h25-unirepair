4dbe442bd28d07c57f0c9e725f619dd9de444719
CoreJust <egor2105094ilin@gmail.com>
2025-04-28 18:10:38 +0300

    Added DB volumes, improved server persistance upon errors

--- clean_run.cmd
+++ clean_run.cmd
@@ -8,6 +8,6 @@ if "%~1"=="-FIXED_CTRL_C" (
    GOTO :EOF
 )
 
-set MODE=prod
+set MODE=clean_run
 docker compose up --build --no-deps -d db
 docker compose up --build --force-recreate --no-deps js
--- src/api_router.js
+++ src/api_router.js
@@ -3,80 +3,81 @@ import * as assert from "./assert.js";
 import { QueryScheme } from "./query_scheme.js";
 
 class ApiRoute {
-  constructor(scheme, handler) {
-    assert.assertClass(scheme, QueryScheme);
-    assert.assertFunction(handler);
-    this.scheme = scheme;
-    this.handler = handler;
-  }
+	constructor(scheme, handler) {
+		assert.assertClass(scheme, QueryScheme);
+		assert.assertFunction(handler);
+		this.scheme = scheme;
+		this.handler = handler;
+	}
 
-  async apply(rawQuery, prehandledState) {
-    assert.assertObject(rawQuery);
-    const query = this.scheme.applyScheme(rawQuery);
-    console.log(
-      `GET with query: ${JSON.stringify(rawQuery)} -> ${JSON.stringify(query)}`,
-    );
-    if (query.constructor === "".constructor) {
-      console.log(
-        `Failed to process query: ${query}\nQuery was: ${JSON.stringify(rawQuery)}`,
-      );
-      return { error: "Internal server error" };
-    }
-    return await this.handler(query, prehandledState);
-  }
+	async apply(rawQuery, prehandledState) {
+		assert.assertObject(rawQuery);
+		const query = this.scheme.applyScheme(rawQuery);
+		console.log(
+			`GET with query: ${JSON.stringify(rawQuery)} -> ${JSON.stringify(query)}`,
+		);
+		if (query.constructor === "".constructor) {
+			console.log(
+				`Failed to process query: ${query}\nQuery was: ${JSON.stringify(rawQuery)}`,
+			);
+			return { error: "Internal server error" };
+		}
+		return await this.handler(query, prehandledState);
+	}
 }
 
 export default class ApiRouter {
-  constructor(apiName, routes) {
-    assert.assertString(apiName);
-    assert.assertObject(routes);
-    let apiRoutes = {};
-    for (const [route, handler] of Object.entries(routes)) {
-      assert.assertString(route);
-      assert.assertFunction(handler);
-      const [name, scheme] = route.includes("/")
-        ? route.split("/")
-        : [route, ""];
-      apiRoutes[name] = new ApiRoute(new QueryScheme(scheme), handler);
-    }
-    this.apiName = apiName;
-    this.apiRoutes = apiRoutes;
-  }
+	constructor(apiName, routes) {
+		assert.assertString(apiName);
+		assert.assertObject(routes);
+		let apiRoutes = {};
+		for (const [route, handler] of Object.entries(routes)) {
+			assert.assertString(route);
+			assert.assertFunction(handler);
+			const [name, scheme] = route.includes("/")
+				? route.split("/")
+				: [route, ""];
+			apiRoutes[name] = new ApiRoute(new QueryScheme(scheme), handler);
+		}
+		this.apiName = apiName;
+		this.apiRoutes = apiRoutes;
+	}
 
-  async runTestQuery(query) {
-    assert.assertString(query);
-    let [path, params] = query.split("?");
-    assert.assert(this.apiRoutes[path], `No such route: ${path}`);
-    let queryObj = {};
-    if (params) {
-      for (const p of params.split("&")) {
-        const [k, v] = p.split("=");
-        queryObj[k] = v;
-      }
-    }
-    return await this.apiRoutes[path].apply(queryObj);
-  }
+	async runTestQuery(query) {
+		assert.assertString(query);
+		let [path, params] = query.split("?");
+		assert.assert(this.apiRoutes[path], `No such route: ${path}`);
+		let queryObj = {};
+		if (params) {
+			for (const p of params.split("&")) {
+				const [k, v] = p.split("=");
+				queryObj[k] = v;
+			}
+		}
+		return await this.apiRoutes[path].apply(queryObj);
+	}
 
-  toExpressRouter(isReady) {
-    assert.assertFunction(isReady);
-    const result = express.Router();
-    for (const [name, route] of Object.entries(this.apiRoutes)) {
-      result.get(`/${this.apiName}/${name}`, async (req, res) => {
-        if (!isReady()) {
-          console.log("Got a query, but the server is not ready yet");
-          res.send({ error: "Internal server error" });
-        }
-        try {
-          let answer = await route.apply(req.query);
-          res.send(answer);
-        } catch (err) {
-          console.error(
-            `Error occured when proccessing route /${this.apiName}/${name}\nError: ${err}\nQuery: ${JSON.stringify(req.query)}`,
-          );
-          res.send({ error: "Internal server error" });
-        }
-      });
-    }
-    return result;
-  }
+	toExpressRouter(isReady) {
+		assert.assertFunction(isReady);
+		const result = express.Router();
+		for (const [name, route] of Object.entries(this.apiRoutes)) {
+			result.get(`/${this.apiName}/${name}`, async (req, res) => {
+				if (!isReady()) {
+					console.log("Got a query, but the server is not ready yet");
+					res.send({ error: "Server not ready" });
+					return;
+				}
+				try {
+					let answer = await route.apply(req.query);
+					res.send(answer);
+				} catch (err) {
+					console.error(
+						`Error occured when proccessing route /${this.apiName}/${name}\nError: ${err}\nQuery: ${JSON.stringify(req.query)}`,
+					);
+					if (!res.headersSent) res.send({ error: "Internal server error" });
+				}
+			});
+		}
+		return result;
+	}
 }
